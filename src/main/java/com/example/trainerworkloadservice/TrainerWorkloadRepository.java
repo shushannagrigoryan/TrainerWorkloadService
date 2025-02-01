bb387d1ecf7510116c6113c4d50c3936e1031634
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2025-02-01 19:13:36 +0400

    Removed redundant code.

--- src/main/java/com/example/trainerworkloadservice/TrainerWorkloadRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.example.trainerworkloadservice;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface TrainerWorkloadRepository extends JpaRepository<TrainerWorkload, String> {
-//}
