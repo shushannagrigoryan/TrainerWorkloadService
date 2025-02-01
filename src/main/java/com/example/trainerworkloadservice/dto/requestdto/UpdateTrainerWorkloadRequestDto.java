0f351c2126bfcfd9a85eb94dd6b823706fff83f4
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2025-02-01 19:10:22 +0400

    Update trainer workload functionality. Get trainer workload.

---
+++ src/main/java/com/example/trainerworkloadservice/dto/requestdto/UpdateTrainerWorkloadRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.trainerworkloadservice.dto.requestdto;
+
+import com.example.trainerworkloadservice.ActionType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UpdateTrainerWorkloadRequestDto {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private Boolean isActive;
+    private LocalDateTime trainingDate;
+    private BigDecimal trainingDuration;
+    private ActionType actionType;
+}
