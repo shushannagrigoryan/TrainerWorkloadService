b3f6d2fb60121fff9eab93281734ccd5dec642ff
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2025-02-03 20:44:02 +0400

    Implemented circuit breaker for fault tolerance.

--- src/main/java/com/example/trainerworkloadservice/controller/TrainerWorkloadController.java
+++ src/main/java/com/example/trainerworkloadservice/controller/TrainerWorkloadController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class TrainerWorkloadController {
     /**
      * update workload.
      */
-    @PostMapping("/updateWorkload")
+    @PutMapping("/workload")
     public ResponseEntity<ResponseDto<String>> updateTrainerWorkload(
         @RequestBody UpdateTrainerWorkloadRequestDto request) {
         trainerWorkloadService.updateTrainerWorkload(
